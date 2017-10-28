071c3deefab4a362ee6d8ddea18a18060b557491
Marietta Pruuli <marietta.pruuli@gmail.com>
2017-10-28 18:52:50 +0300

    Homework: variables, random numbers, if-statements, while loop, break

---
+++ src/02_homework/Exercises/02_guessDefinedNumberEquals/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_guessDefinedNumberEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int predefinedNumber = 42;
+
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata.");
+            Console.WriteLine();
+            Console.Write("Sisesta number: ");
+
+            string input = Console.ReadLine();
+            int userNumber = int.Parse(input);
+
+            if (predefinedNumber > userNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Arvuti valitud number on suurem");
+            }
+            if (predefinedNumber < userNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Arvuti valitud number on väiksem");
+            }
+            if (predefinedNumber == userNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Arvasid numbri ära!");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/02_homework/Exercises/05_guessRandomNumberWhileBreak/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_guessRandomNumberWhileBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 101);
+
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata.");
+
+            bool haventGuessed = true;
+            while (haventGuessed)
+            {
+                Console.WriteLine();
+                Console.Write("Sisesta number: ");
+
+                string input = Console.ReadLine();
+                int userNumber = int.Parse(input);
+
+                if (randomNumber > userNumber)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Arvuti valitud number on suurem");
+                }
+                if (randomNumber < userNumber)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Arvuti valitud number on väiksem");
+                }
+                if (randomNumber == userNumber)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Arvasid numbri ära!");
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
