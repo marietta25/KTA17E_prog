0e947e007486b6bfc05b1421fab34fee017e2c54
Marietta Pruuli <marietta.pruuli@gmail.com>
2017-10-28 19:07:07 +0300

    Fix first exercise

---
+++ src/02_homework/Exercises/01_guessPredefinedNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_guessPredefinedNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int predefinedNumber = 42;
+
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata.");
+            Console.WriteLine();
+            Console.Write("Sisesta number: ");
+
+            string input = Console.ReadLine();
+            int userNumber = int.Parse(input);
+
+            if (predefinedNumber > userNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Arvuti valitud number on suurem");
+            }
+            if (predefinedNumber < userNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Arvuti valitud number on väiksem");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
