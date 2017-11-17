325d4090c22da2f8343e4ffad3d61345d107f127
Marietta Pruuli <marietta.pruuli@gmail.com>
2017-11-17 17:25:21 +0200

    Try out methods

---
+++ src/05_class/Exercises/02_method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSomeThing("midagi");
+            PrintWord();
+            Console.ReadLine();
+        }
+        static void DoSomeThing(string word)
+        {
+            Console.WriteLine($"Teen {word}");
+        }
+        static string PrintWord()
+        {
+            Console.Write("Kirjuta natuke: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Sa kirjutasid {input}");
+            return input;
+        }
+    }
+}
