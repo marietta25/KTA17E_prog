e3e7f0e1016f91a6ccb9f7ba0021943d61db839b
Marietta Pruuli <marietta.pruuli@gmail.com>
2017-11-17 13:51:22 +0200

    Warmup exercise

---
+++ src/05_class/WarmUp/Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Tere, teeme programmeerimiseks soojendust");
+            Console.WriteLine("Kirjuta mulle midagi: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"Selge - {input}");
+            Console.ReadLine();
+        }
+    }
+}
