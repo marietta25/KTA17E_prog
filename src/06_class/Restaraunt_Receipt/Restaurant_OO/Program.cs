50c17f204e60bcfd60b264779331b01e85aeba87
marietta25 <32923340+marietta25@users.noreply.github.com>
2018-01-15 15:29:32 +0200

    Hangman game

---
+++ src/06_class/Restaraunt_Receipt/Restaurant_OO/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_OO
+{
+    class Restaurant
+    {
+        private string _name;   // alakriipsuga nimetatakse privaatseid muutujaid
+        private string _address;
+        public Restaurant(string name, string address)
+        {
+            this._name = name;
+            this._address = address;
+        }
+        public Receipt GetReceipt(Tab tab)
+        {
+            return new Receipt(_name, _address);
+        }
+    }
+    class Tab
+    {
+        public IEnumerable<double> Entries { get; internal set; }
+        public void Add(double price)
+        {
+            Entries.Add(price);
+        }
+
+    }
+    class Receipt
+    {
+        private string _restaurantName;
+        private string _restaurantAddress;
+        private Tab _tab;
+
+        public Receipt(string restaurantName, string restaurantAddress, Tab tab)
+        {
+            this._restaurantName = restaurantName;
+            this._restaurantAddress = restaurantAddress;
+            this._tab = tab;
+        }
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            foreach (var entry in _tab.Entries)
+            {
+                str.AppendLine($"Price of food item: {entry}");
+            }
+            
+            str.AppendLine($"SubTotal: €{_tab.Entries.Sum()}");
+            str.AppendLine($"15% Gratuity: €{_tab.Entries} ");
+            str.AppendLine($"Total: €{_tab.Entries} ");
+            return str.ToString();
+        }
+
+    }
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Restaurant restaraunt = new Restaurant("Taco Palenque", "1200 Main ST.");
+
+            Tab tab = new Tab();
+
+            tab.Add(7.99);
+            tab.Add(6.55);
+            tab.Add(2.345);
+
+            Receipt receipt = restaraunt.GetReceipt(tab);
+
+            Console.Write(receipt);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ src/06_class/Restaraunt_Receipt/WordGuessingGame/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mingi arv etteantud sõnu
+            //arvuti valib suvaliselt sõna
+            //mängija pakub tähti
+            //kindel arv kordi valesti pakkumisest
+
+            Console.WriteLine("Valin välja ühe sõna.");
+            Console.WriteLine("Sina hakka tähti pakkuma.");
+            Console.WriteLine();
+
+            List<string> words = new List<string> { "raamatukogu", "taevas", "rohutirts", "ploomipuu", "linnapea", "kassipoeg", "suurettevõte", "aplikatsioon", "universum", "korsten"};
+
+            int random = new Random().Next(0, words.Count);
+            string word = words[random];
+
+            //mitu tähte sõnas
+            int characters = word.Length;
+
+            //prindi välja kriipsud
+            for (var i = 1; i <= characters; i++)
+            {
+                Console.Write("_ ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Paku täht: ");
+
+            string character = Console.ReadLine();
+            List<string> userInput = new List<string>();
+            userInput.Add(character);
+
+            var foundIndexes = new List<int>();
+
+
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (word.IndexOf(character) == -1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Pakkusid valesti");
+                }
+                else
+                {
+                    Console.WriteLine("Pakkusid õigesti");
+                    foundIndexes.Add(word.IndexOf(character));
+                }
+            }
+
+            
+
+
+
+
+            Console.WriteLine(word);
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
