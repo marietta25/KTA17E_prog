dc7535d2a3d38eb440e37ddbca101113f04517b0
marietta25 <32923340+marietta25@users.noreply.github.com>
2018-02-12 16:41:28 +0200

    Roll dices (obj oriented)

---
+++ src/08_class/rollDice/rollDice/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rollDice
+{
+    class Dice
+    {
+        private Random rand = new Random();
+
+        public int Roll(int sides)
+        {
+            int rolled = rand.Next(1, sides+1);
+            return rolled;
+        }
+    }
+}
---
+++ src/08_class/rollDice/rollDice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rollDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+
+            int rolledSide;
+            int sum = 0;
+
+            Console.WriteLine("/roll 3d6 2d8");
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                rolledSide = dice.Roll(6);
+                Console.WriteLine($"1d6: {rolledSide}");
+                sum += rolledSide;
+            }
+            
+            for (int i = 0; i < 2; i++)
+            {
+                rolledSide = dice.Roll(8);
+                Console.WriteLine($"1d8: {rolledSide}");
+                sum += rolledSide;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Roll total: {sum}");
+            Console.WriteLine();
+            Console.Write("> ");
+
+            Console.ReadKey();
+        }
+    }
+}
